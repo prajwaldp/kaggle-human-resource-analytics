635814fc6385b6317cec6b3ecf3b6cb83207faec
Prajwal Prakash <prajwalpy6@gmail.com>
2017-07-23 08:55:00 +0530

    Add the statistical modeling files for last_evaluation and satisfaction_levels

---
+++ statistical_modeling/last_evaluation.py
@@ -0,0 +1,41 @@
+# checking out the distribution of the `last_evaluation` column
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv("cleaned_df.csv")
+mean = np.mean(df["last_evaluation"])
+std = np.std(df["last_evaluation"])
+
+
+# Function to compute the ECDF
+def ecdf(data):
+    n = len(data)
+    x = np.sort(data)
+    y = np.arange(1, n + 1) / n
+    return x, y
+
+
+# trying out a normal distribution
+samples = np.random.normal(mean, std, size=10000)
+x, y = ecdf(df["last_evaluation"])
+x_theor, y_theor = ecdf(samples)
+_ = plt.plot(x_theor, y_theor)
+_ = plt.plot(x, y, marker='.', linestyle='none')
+plt.xlabel("Last Evaluation")
+plt.ylabel("CDFs")
+plt.show()
+
+
+# trying out an exponential distribution
+samples = np.random.exponential(mean, size = 10000)
+x, y = ecdf(df["last_evaluation"])
+x_theor, y_theor = ecdf(samples)
+_ = plt.plot(x_theor, y_theor)
+_ = plt.plot(x,y,marker = '.', linestyle = 'none')
+plt.xlabel("Last Evaluation")
+plt.ylabel("CDFs")
+plt.show()
---
+++ statistical_modeling/satisfaction_level.py
@@ -0,0 +1,41 @@
+# checking out the distribution of the `satisfaction_level` column
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv("cleaned_df.csv")
+mean = np.mean(df["satisfaction_level"])
+std = np.std(df["satisfaction_level"])
+
+
+# Function to compute the ECDF
+def ecdf(data):
+    n = len(data)
+    x = np.sort(data)
+    y = np.arange(1, n + 1) / n
+    return x, y
+
+
+# trying out a normal distribution
+samples = np.random.normal(mean, std, size=10000)
+x, y = ecdf(df["satisfaction_level"])
+x_theor, y_theor = ecdf(samples)
+_ = plt.plot(x_theor, y_theor)
+_ = plt.plot(x, y, marker='.', linestyle='none')
+plt.xlabel("Satisfaction Levels")
+plt.ylabel("CDFs")
+plt.show()
+
+
+# trying out an exponential distribution
+samples = np.random.exponential(mean, size = 10000)
+x, y = ecdf(df["satisfaction_level"])
+x_theor, y_theor = ecdf(samples)
+_ = plt.plot(x_theor, y_theor)
+_ = plt.plot(x,y,marker = '.', linestyle = 'none')
+plt.xlabel("Satisfaction Levels")
+plt.ylabel("CDFs")
+plt.show()
