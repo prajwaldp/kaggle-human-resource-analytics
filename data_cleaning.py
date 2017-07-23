340907e20a0202fce53d4dc143594853d4b330b8
Prajwal Prakash <prajwalpy6@gmail.com>
2017-07-23 10:22:26 +0530

    Rename average_montly_hours to average_monthly_hours

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,12 +4,19 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("HR_comma_sep.csv")
 
+
 # rename `Work_accident` to `work_accident`
 df['work_accident'] = df['Work_accident']
 df = df.drop('Work_accident', 1)
 
+# rename `average_montly_hours` to `average_monthly_hours`
+df['average_monthly_hours'] = df['average_montly_hours']
+df = df.drop('average_montly_hours', 1)
+
+
 # No na's found
 # df.isnull().sum()
+
 
 # Convert required columns to categorical
 df['salary'] = df['salary'].astype('category').cat.codes
